49abea11ed964337bd4333085a8ce50cc628b72d
Josh <joshgbuckner@gmail.com>
2019-10-20 19:16:23 -0500

    new backend sends 50 comments at a time, connect websockets, new message animation

--- src/containers/Thread/Thread.tsx
+++ src/containers/Thread/Thread.tsx
@@ -11,49 +11,80 @@ interface Params {
   threadSlug?: string
 }
 
-let commentsList:any = []
 
-const getComments = (comment:any) => {
-  if (!comment) {
-    return
-  }
-  commentsList.push(comment)
-  if (comment.data.replies.data.children) {
-    comment.data.replies.data.children.forEach((reply:any) => getComments(reply))
-  }
-  return
-}
+
+let scrollPosition = 0
+
 
 const Thread: React.FC = () => {
-  const [comments, setComments] = useState()
+  const [comments, setComments] = useState<any>([])
   const { subreddit, threadID, threadSlug } :Params = useParams()
+
   useEffect(() => {
     const socket = new WebSocket('ws://localhost:8080/ws')
     socket.onopen = () => {
       socket.send(JSON.stringify({subSlug: subreddit, threadID: threadID, threadSlug: threadSlug, commentID: threadID}))
     }
     socket.onmessage = (event) => {
-      // setComments(JSON.parse(event.data))
-      setComments((prevComments:any) => {
-        return {...prevComments, ...JSON.parse(event.data)}
-      })
+
+      const newComments = JSON.parse(event.data)
+      const commentsList:any = []
+
+      const getComments = (comment:any) => {
+        if (!comment) {
+          return
+        }
+        commentsList.push(comment)
+        if (comment.data.replies.data.children) {
+          comment.data.replies.data.children.forEach((reply:any) => getComments(reply))
+        }
+        return
+      }
+
+      if (newComments) {
+        newComments.data.children.forEach((comment:any) => getComments(comment))
+      }
+
+      // setComments((prevComments:any) => {
+      //   return {...prevComments, ...commentsList}
+      // })
+
+      setComments([...commentsList, ...comments])
+
+      console.log(comments)
+
+      // setComments(commentsMerged)
+
+      console.log('message recieved')
+      console.log(scrollPosition)
+      
+      let { scrollTop, scrollHeight } = document.documentElement
+      scrollTop = scrollTop >= scrollHeight ? scrollHeight : scrollPosition
     }
     return () => {
       socket.close()
     }
   },[subreddit, threadID, threadSlug])
 
-  if (comments) {
-    comments.data.children.forEach((comment:any) => getComments(comment))
-  }
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log(document.documentElement.scrollTop)
+      let { scrollTop } = document.documentElement
+      scrollPosition = scrollTop
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  },[])
 
   return (
     comments ? 
       <div className="thread">
-        {commentsList.map((comment:any, index:number) => 
+        {comments.map((comment:any, index:number) => 
           comment.kind !== 'more' ?
             <Comment 
-              key={index} 
+              key={comment.data.id} 
               comment={comment.data.body_html} 
               author={comment.data.author}
               created={comment.data.created_utc}
@@ -61,7 +92,7 @@ const Thread: React.FC = () => {
             /> 
           :
           <ReplyTrunc 
-            key={index} 
+            key={comment.data.id} 
             depth={comment.data.depth}
           />
         )}
